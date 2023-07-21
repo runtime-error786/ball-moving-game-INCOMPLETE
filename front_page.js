1bcd1af289c855b0e885ad6bcc58e050e51d5326
Mustafa Rizwan <mustafa782a@gmail.com>
2023-07-21 10:53:50 +0500

    boundary detection

--- front_page.js
+++ front_page.js
@@ -1,27 +1,57 @@
 let tb = 33;
 let lr = 30;
 let r = document.getElementById("ball");
+let topb = 33;
+let bot = 665;
+let left = 30;
+let rig = 1470;
 document.addEventListener("keypress" , function(event)
 {
  if(event.key == "w")
  {
-  tb = tb - 5;
-  r.style.top = tb+"px";
+    tb = tb - 5;
+    if(tb>topb)
+    {
+        r.style.top = tb+"px";
+    }
+    else{
+        tb = tb + 5;
+    }
  }
  else if(event.key == "s")
  {
     tb = tb + 5;
-    r.style.top = tb+"px";
+    if(tb<bot)
+    {
+        r.style.top = tb+"px";
+    }
+    else{
+        tb = tb - 5;
+    }
+    
  }
  else if(event.key == "a")
  {
     lr = lr - 5;
-    r.style.left = lr +"px";
+    if(lr>left)
+    {
+        r.style.left = lr +"px";
+    }
+    else{
+        lr = lr + 5;
+    }
  }
  else if(event.key == "d")
 {
     lr = lr + 5;
-    r.style.left = lr +"px";
+
+    if(lr<rig)
+    {
+        r.style.left = lr +"px";
+    }
+    else{
+        lr = lr - 5;
+    }
 }
 });
 // only use left and top position 
